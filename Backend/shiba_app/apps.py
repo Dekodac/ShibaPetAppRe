9fca64f72d99ff34b8b1ba624a950147fdf2bf1c
Dakota Cannon <Dekodac@gmail.com>
2023-08-25 21:02:49 -0400

    added .gitignore

---
+++ Backend/shiba_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShibaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shiba_app'
---
+++ Backend/shiba_pet_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShibaPetAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shiba_pet_app'
