ec565f8c50384fe2a17b43ac5d34171c1780a135
Dakota Cannon <Dekodac@gmail.com>
2023-08-25 22:10:07 -0400

    React side brought back up to OG version with some styling errors as collateral damage for breaking the first repo

---
+++ Frontend/shibapetappre/src/components/ShibaCard.jsx
@@ -0,0 +1,5 @@
+export default function ShibaCard(){
+    return (
+        <div>hello THIS IS A SHIBA CARD</div>
+    )
+}
---
+++ Frontend/shibapetappre/src/router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter} from 'react-router-dom';
+import App from './App';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import SignUpPage from './components/SignUpPage';
+import LoginPage from './components/LoginPage';
+import AccountPage from './components/AccountPage';
+
+      
+      const router = createBrowserRouter([
+        {
+          path: '/',
+          element: <App />,
+        //   errorElement: <Error404Page/>,
+          children: [
+            {
+              index: true,
+              element: <HomePage />,
+            },
+            {
+              path: '/about',
+              element: <AboutPage />
+            },
+            {
+              path: '/signup',
+              element: <SignUpPage />
+            },
+            {
+              path: '/login',
+              element: <LoginPage />
+            },
+            {
+            path: '/account',
+            element: <AccountPage />
+            }
+          ],
+        },
+      ],);
+      
+      export default router;
+      
