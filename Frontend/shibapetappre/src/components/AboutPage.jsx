2d45a742441a8fcac8ad163fff18ef029ae482ff
Dakota Cannon <Dekodac@gmail.com>
2023-08-29 20:32:31 -0400

    added favicon of shibe screaming for webpage icon, can't get it to work.... not a priority, set up inventory page and creation page with toggles and text fields and color pickers with state functionality.

--- Frontend/shibapetappre/src/components/AboutPage.jsx
+++ Frontend/shibapetappre/src/components/AboutPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ShibaGifPicture from "../assets/eva-sanchez-purple-shibabueno.gif";
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { LogoutContext } from './LogoutContext';
 
 export default function AboutPage() {
+  const {handleLogout} = useContext(LogoutContext)
+
+  
   return (
 <div style={{width: 1512, height: 982, position: 'relative', background: '#BEB378'}}>
     <div style={{width: 1512, height: 65, left: 0, top: 914, position: 'absolute'}}>
@@ -22,6 +27,10 @@ export default function AboutPage() {
     </div>
     <div style={{width: 864, height: 370, left: 324, top: 113, position: 'absolute', background: 'rgba(255, 240.12, 240.12, 0.78)', borderRadius: 34, border: '0.50px black solid'}} />
     <div style={{width: 826, height: 270, left: 345, top: 140, position: 'absolute', textAlign: 'center'}}><span style={{color: 'black', fontSize: 32, fontFamily: 'Indie Flower', fontWeight: '400', wordWrap: 'break-word'}}>Hi, My name is Dakota and have a small place in my heart for shiba inus so I decided to build a character game for them! </span><span style={{color: 'black', fontSize: 40, fontFamily: 'Indie Flower', fontWeight: '400', wordWrap: 'break-word'}}>O</span><span style={{color: 'black', fontSize: 32, fontFamily: 'Indie Flower', fontWeight: '400', wordWrap: 'break-word'}}>nce you sign up, you can create a shiba with different attributes and will be given a shiba character based on the attributes you gave! Pretty cool huh? Try it out now! Each shiba can get up to 100 experience points, and unlock several tricks to perform!</span></div>
+    <Link to={"/"} onClick={handleLogout}>
+        <div className="BackToInventoryDiv" style={{width: 326, height: 74, left: 0, top: 0, position: 'absolute', background: '#D2DBA0', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 70}} />
+        <div className="Logout" style={{width: 275.34, height: 37.84, left: 40, top: 18.50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Kiwi Maru', fontWeight: '500', wordWrap: 'break-word'}}>Logout<br/></div>
+        </Link>
 </div>
   );
 }
--- Frontend/shibapetappre/src/components/ShibaCreationPage.jsx
+++ Frontend/shibapetappre/src/components/ShibaCreationPage.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { LogoutContext } from "./LogoutContext";
+import { useState } from "react";
+
+
 export default function ShibaCreationPage(){
+    const {handleLogout} = useContext(LogoutContext)
+
+
+    const [isCaneEquipped, setIsCaneEquipped] = useState(false);
+    const [hasDramaticMustache, setHasDramaticMustache] = useState(false);
+    const [isHairClipWorn, setIsHairClipWorn] = useState(false);
+    const [isMuzzleWorn, setIsMuzzleWorn] = useState(false);
+    const [areGlassesWorn, setAreGlassesWorn] = useState(false);
+    const [isBowTieWorn, setIsBowTieWorn] = useState(false);
+    const [isTopHatWorn, setIsTophatWorn] = useState(false);
+    
+    const [furColor, setFurColor] = useState('#000000'); // Default black color
+    const [eyeColor, setEyeColor] = useState('#000000'); // Default black color
+    const [name, setName] = useState(''); // Default empty name
+
+
+
     const token = localStorage.getItem('token');
   if (token === "undefined"){
     localStorage.removeItem("token");
+  }
+
+  function handleCreate(){
+    alert("you created a shibe")
   }
 
 
@@ -24,54 +51,56 @@ export default function ShibaCreationPage(){
     <div style={{width: 70, height: 46, left: 721, top: 78, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 128, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>?</div>
     <div className="Group1" style={{width: 399, height: 88, left: 556, top: 711, position: 'absolute'}}>
         <div className="BackToInventoryDiv" style={{width: 399, height: 88, left: 0, top: 0, position: 'absolute', background: '#D2DBA0', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 70}} />
-        <div className="Create" style={{width: 337, height: 45, left: 31, top: 22, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Kiwi Maru', fontWeight: '500', wordWrap: 'break-word'}}>CREATE</div>
+        <div className="Create" style={{width: 337, height: 45, left: 31, top: 15, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Kiwi Maru', fontWeight: '500', wordWrap: 'break-word'}} onClick={handleCreate}><button>CREATE</button></div>
     </div>
     <div className="ShibaAttributesGroup" style={{width: 743, height: 295, left: 410, top: 344, position: 'absolute'}}>
         <div className="OwnedShibaCardDetails" style={{width: 743, height: 295, left: 0, top: 0, position: 'absolute', background: '#D9D9D9'}} />
         <div className="CaneGroup" style={{width: 319, height: 39, left: 381, top: 230, position: 'absolute'}}>
             <div className="Cane" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="CaneTextField" style={{width: 282, height: 29, left: 27, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Cane equipped: yes/no</div>
+            <div className="CaneTextField" style={{width: 282, height: 29, left: 27, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Cane equipped:<input type="checkbox" checked={isCaneEquipped} onChange={() => setIsCaneEquipped(!isCaneEquipped)} /></div>
         </div>
         <div className="DramaticMustacheGroup" style={{width: 319, height: 39, left: 381, top: 176, position: 'absolute'}}>
             <div className="DramaticMustache" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="DaramaticMustacheField" style={{width: 302, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Dramatic Mustache: yes/no</div>
+            <div className="DaramaticMustacheField" style={{width: 302, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Dramatic Mustache: <input type="checkbox" checked={hasDramaticMustache} onChange={() => setHasDramaticMustache(!hasDramaticMustache)} /></div>
         </div>
         <div className="HairBowGroup" style={{width: 319, height: 39, left: 381, top: 122, position: 'absolute'}}>
             <div className="HairBow" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="HairclipField" style={{width: 261, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Hair-clip worn/notworn</div>
+            <div className="HairclipField" style={{width: 261, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Hair-clip:<input type="checkbox" checked={isHairClipWorn} onChange={() => setIsHairClipWorn(!isHairClipWorn)} /></div>
         </div>
         <div className="MuzzleGroup" style={{width: 319, height: 39, left: 381, top: 68, position: 'absolute'}}>
             <div className="Muzzle" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="MuzzleField" style={{width: 260, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Muzzle worn/notworn</div>
+            <div className="MuzzleField" style={{width: 260, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Muzzle<input type="checkbox" checked={isMuzzleWorn} onChange={() => setIsMuzzleWorn(!isMuzzleWorn)} /></div>
         </div>
         <div className="GlassesGroup" style={{width: 319, height: 39, left: 381, top: 14, position: 'absolute'}}>
             <div className="Glasses" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="GlassesField" style={{width: 260, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Glasses worn/notworn</div>
+            <div className="GlassesField" style={{width: 260, height: 29, left: 17, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Glasses:<input type="checkbox" checked={areGlassesWorn} onChange={() => setAreGlassesWorn(!areGlassesWorn)} /></div>
         </div>
         <div className="BowtieGroup" style={{width: 319, height: 39, left: 21, top: 230, position: 'absolute'}}>
             <div className="Bowtie" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="BowtieField" style={{width: 252, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>BowTie worn/notworn</div>
+            <div className="BowtieField" style={{width: 252, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>BowTie<input type="checkbox" checked={isBowTieWorn} onChange={() => setIsBowTieWorn(!isBowTieWorn)} /></div>
         </div>
         <div className="TopHatGroup" style={{width: 319, height: 39, left: 21, top: 176, position: 'absolute'}}>
             <div className="Tophat" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="TophatField" style={{width: 250, height: 29, left: 27, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Tophat worn/notworn</div>
+            <div className="TophatField" style={{width: 250, height: 29, left: 27, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Tophat: <input type="checkbox" checked={isTopHatWorn} onChange={() => setIsTophatWorn(!isTopHatWorn)} /></div>
         </div>
         <div className="FurColorGroup" style={{width: 319, height: 39, left: 21, top: 122, position: 'absolute'}}>
             <div className="FurColor" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="FurColorField" style={{width: 234, height: 29, left: 43, top: 2, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Fur Color<br/></div>
+            <div className="FurColorField" style={{width: 234, height: 29, left: 43, top: 2, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Fur Color:<input type="color" style={{width: 50, height: 35}}value={furColor} onChange={(e) => setFurColor(e.target.value)} /></div>
         </div>
         <div className="EyeColorGroup" style={{width: 319, height: 39, left: 21, top: 68, position: 'absolute'}}>
             <div className="Eyecolor" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="EyeColorField" style={{width: 234, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>EyeColor:</div>
+            <div className="EyeColorField" style={{width: 234, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>EyeColor:<input type="color" style={{width: 50, height: 35}}value={eyeColor} onChange={(e) => setEyeColor(e.target.value)} /></div>
         </div>
         <div className="NameGroup" style={{width: 319, height: 39, left: 21, top: 14, position: 'absolute'}}>
             <div className="Name" style={{width: 319, height: 39, left: 0, top: 0, position: 'absolute', background: '#3D70BD', borderRadius: 118}} />
-            <div className="Name" style={{width: 234, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Name</div>
+            <div className="Name" style={{width: 234, height: 29, left: 43, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Name: <input type="text" value={name} onChange={(e) => setName(e.target.value)} /></div>
         </div>
     </div>
-    <div className="UserInventoryHeaderGroup" style={{width: 326, height: 74, left: 1153, top: 27, position: 'absolute'}}>
+    <div className="UserInventoryHeaderGroup" style={{width: 326, height: 74, left: 1153, top: 5, position: 'absolute'}}>
+        <Link to={"/"} onClick={handleLogout}>
         <div className="BackToInventoryDiv" style={{width: 326, height: 74, left: 0, top: 0, position: 'absolute', background: '#D2DBA0', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 70}} />
-        <div className="Logout" style={{width: 275.34, height: 37.84, left: 25.33, top: 18.50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Kiwi Maru', fontWeight: '500', wordWrap: 'break-word'}}>Logout<br/></div>
+        <div className="Logout" style={{width: 275.34, height: 37.84, left: 40, top: 18.50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Kiwi Maru', fontWeight: '500', wordWrap: 'break-word'}}>Logout<br/></div>
+        </Link>
     </div>
 </div>
     )
