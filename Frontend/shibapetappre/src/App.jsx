e97027fe068ea50ef01253aba1933377dc9b33c7
Dakota Cannon <Dekodac@gmail.com>
2023-08-29 23:22:48 -0400

    created api for random dog facts and a context for it to be grabbed across the components. styling needs alot of help..

--- Frontend/shibapetappre/src/App.jsx
+++ Frontend/shibapetappre/src/App.jsx
@@ -1,63 +1,83 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {  Outlet } from 'react-router-dom';
 import { LoginContext } from './components/LoginContext';
 import axios from 'axios';
 import { LogoutContext } from './components/LogoutContext';
+import { DogFactContext }  from './components/DogFactContext';
+
 
 export default function App() {
   
-const [loginStatus, setLoginStatus] = useState(null);
-const [errorMessage, setErrorMessage] = useState('');
-const [email, setEmail] = useState('');
-const [passw, setpassw] = useState('');
-const navigate = useNavigate();
-const location = useLocation();
-const lastVisited = useRef();
-const [user, setUser] = useState(null);
+  const [loginStatus, setLoginStatus] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [passw, setpassw] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const lastVisited = useRef();
+  const [user, setUser] = useState(null);
+  const [dogFact, setDogFact] = useState("");
 
-const whoAmI = async () => {
-  // Check if a token is stored in the localStorage
-  let token = localStorage.getItem("token");
-  if (token) {
-    // If the token exists, set it in the API headers for authentication
-    // api.defaults.headers.common["Authorization"] = `Token ${token}`;
-    // Fetch the user data from the server using the API
-    let response = await axios.get('http://127.0.0.1:8000/account/details/', {
-      headers: {
-        Authorization: `Token ${token}`,
-      },
-    });
-    console.log(response)
-    // Check if the response contains the user data (email field exists)
-    if (response.data.email) {
-      // Set the user data in the context or state (assuming `setUser` is a state update function)
-      setUser(response.data);
-      // If the user is authenticated and there is a stored lastVisited page,
-      // navigate to the lastVisited page; otherwise, navigate to the default homepage "/home"
-      // if (lastVisited.current) {
-      //   navigate(lastVisited.current);
-      // } else {
-      //   navigate("/home");
-      // }
-      navigate("/")
-    }
-  } else {
-    // If no token is found, navigate to the home page
-    navigate("/");
-  }
-};
-
-useEffect(() => {
-  whoAmI();
+  
+  useEffect(() => {
+    // Make the API request
+    axios.get('https://dogapi.dog/api/v2/facts')
+        .then(response => {
+            // Extract the text from the "body" attribute of the JSON response
+            const factText = response.data.data[0].attributes.body;
+            // Set the extracted text to the state variable
+            setDogFact(factText);
+        })
+        .catch(error => {
+            console.error('Error fetching dog fact:', error);
+        });
 }, []);
 
-useEffect(() => {
-  if (!user) {
-    // If the user is not authenticated, update the lastVisited ref with the current location pathname
-    lastVisited.current = location.pathname;
-  }
-}, [location]);
+console.log(dogFact)
+
+  const whoAmI = async () => {
+    // Check if a token is stored in the localStorage
+    let token = localStorage.getItem("token");
+    if (token) {
+      // If the token exists, set it in the API headers for authentication
+      // api.defaults.headers.common["Authorization"] = `Token ${token}`;
+      // Fetch the user data from the server using the API
+      let response = await axios.get('http://127.0.0.1:8000/account/details/', {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+      console.log(response)
+      // Check if the response contains the user data (email field exists)
+      if (response.data.email) {
+        // Set the user data in the context or state (assuming `setUser` is a state update function)
+        setUser(response.data);
+        // If the user is authenticated and there is a stored lastVisited page,
+        // navigate to the lastVisited page; otherwise, navigate to the default homepage "/home"
+        // if (lastVisited.current) {
+        //   navigate(lastVisited.current);
+        // } else {
+        //   navigate("/home");
+        // }
+        navigate("/")
+      }
+    } else {
+      // If no token is found, navigate to the home page
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    whoAmI();
+  }, []);
+
+  useEffect(() => {
+    if (!user) {
+      // If the user is not authenticated, update the lastVisited ref with the current location pathname
+      lastVisited.current = location.pathname;
+    }
+  }, [location]);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -66,7 +86,7 @@ useEffect(() => {
   const handlePasswChange = (e) => {
   setpassw(e.target.value);
   };
-  
+    
   const handleLogInSubmit = async (e) => {
       e.preventDefault();
       setEmail("");
@@ -111,11 +131,13 @@ useEffect(() => {
 
   return (
     <>
+    <DogFactContext.Provider value={{dogFact,setDogFact}}>
     <LogoutContext.Provider value={{handleLogout}}>
     <LoginContext.Provider value={{loginStatus,setLoginStatus,handleLogInSubmit,handlePasswChange,handleEmailChange,email,passw}}>
       <Outlet />
     </LoginContext.Provider>
     </LogoutContext.Provider>
+    </DogFactContext.Provider>
     </>
   );
 }
